282dd0bf768b758d9f233affe4e1922a86655ab9
Ana Elizondo-Salas <anaelizondo796@gmail.com>
2025-03-21 17:54:46 -0600

    Update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-const itemsController = new ItemsController(0);
 
+
+itemsController.addItem("floresA", "Flores/img", "Flores Azules");
+itemsController.addItem("floresB", "Flores/img", "Flores Rojas");
+console.log(itemsController.items);
 // Select the New Task Form
 const newItemForm = document.querySelector('#newItemForm');
 
@@ -29,25 +32,3 @@ newItemForm.addEventListener('submit', (event) => {
     newItemDescription.value = '';
 });
 
-
-function addItemCard(item){
-    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
-}
-
-addItemCard({'name':'juice',
-    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-    'description':'Orange and Apple juice fresh and delicious'});
-
-addItemCard({'name':'Tayto',
-    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
-    'description':'Cheese & Onion Chips'})
